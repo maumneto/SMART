2f650f657420323e06a1c9e7f3ae1cf2f9eb28e3
Mauricio Neto <maumneto@gmail.com>
2017-10-19 07:09:55 -0300

    nova versao

--- SMART.ino
+++ SMART.ino
@@ -1,21 +1,28 @@
   
-//SMART - Sistema de Monitoramento de ruídos em SmartCities
-//Autor: Maurício Moreira Neto
+/* ########################################################
+  SMART: Sistema de MonitorAmento de Ruído para smarTcities  
+  Autor: Maurício Moreira Neto
+  Data: 19/10/2017
+ ########################################################## */
 
 //Definicao pinos leds
 int pinoled_ver = 3;
 int pinoled_ama = 4;
 int pinoled_verm = 5;
 
-//valores para o calculo de dB
+//Variaveis para o calculo de dB
 float tensao = 0;
 int dB = 0;
 
 //Definicao pinos sensor
 int pino_analogico = A0;
-
 int valor_A0 = 0;
 
+/*
+  - Iniciando o monitor serial
+  - Definindo os pinos dos LEDs
+  - Definindo o pino analogico do sensor de som KY 038
+*/
 void setup()
 {
   Serial.begin(9600);
@@ -27,18 +34,29 @@ void setup()
   pinMode(pino_analogico, INPUT);
 }
 
+/*
+  - Mostrando os valores no monitor serial
+  - Calculo do nivel de ruido (em dB)
+  - Threshold do valor de som
+*/
 void loop()
 {
   valor_A0 = analogRead(pino_analogico);
-  Serial.print("Saida A0: ");
-  Serial.println(valor_A0);
   tensao = valor_A0/1023.0*4.53;
   dB = 87.1*tensao - 75,4;
+  
+  // Mostrando nos valor no monitor serial 
+  Serial.print("Saida A0: ");
+  Serial.println(valor_A0);
   Serial.print("Ruído: ");
   Serial.println(dB);
   Serial.println("");
   //delay(1000);
   
+  // Threshold dos valores de ruido
+  if(valor_A0 < 0){
+      Serial.println("Erro no sensor!");
+    }
   if (valor_A0 > 0 && valor_A0 < 300){
       digitalWrite(pinoled_ver, HIGH);
       digitalWrite(pinoled_ama, LOW);
@@ -49,7 +67,7 @@ void loop()
       digitalWrite(pinoled_ama, HIGH);
       digitalWrite(pinoled_verm, LOW);
     }
-    if (valor_A0 > 700){
+    if (valor_A0 > 701  ){
       digitalWrite(pinoled_ver, LOW);
       digitalWrite(pinoled_ama, LOW);
       digitalWrite(pinoled_verm, HIGH);
